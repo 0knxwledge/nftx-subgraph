19d0717cb459afce99c79af86c8af074b31be535
0knxwledge <0knxwledge@gmail.com>
2021-03-16 19:29:30 -0400

    add old contracts

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,12 @@
-import { XTokenSet, XTokenAddressSet, IsEligibleSet, NftAddressSet, NftSet, NewVaultAdded, TokenBalanceSet, EthBalanceSet } from '../generated/XStore/XStore'
-import { Mint, MintRequested, NewVault, OwnershipTransferred, Redeem, CreateVaultCall } from '../generated/NFTX/NFTX'
+import { XTokenAddressSet, NftAddressSet, NewVaultAdded} from '../generated/XStore/XStore'
+import { Mint, NewVault, Redeem, CreateVaultCall } from '../generated/NFTXv7/NFTXv7'
+// import { Mint, NewVault, Redeem, CreateVaultCall } from '../generated/NFTX/NFTX'
+// import { Mint, NewVault, Redeem, CreateVaultCall } from '../generated/NFTXv2/NFTXv2'
+// import { Mint, NewVault, Redeem, CreateVaultCall } from '../generated/NFTXv3/NFTXv3'
+// import { Mint, NewVault, Redeem, CreateVaultCall } from '../generated/NFTXv4/NFTXv4'
+// import { Mint, NewVault, Redeem, CreateVaultCall } from '../generated/NFTXv5/NFTXv5'
+// import { Mint, NewVault, Redeem, CreateVaultCall } from '../generated/NFTXv6/NFTXv6'
+// import { Mint, NewVault, Redeem, CreateVaultCall } from '../generated/NFTXv7/NFTXv7'
 
 import { Vault, MintEntity, RedeemEntity } from '../generated/schema'
 
@@ -9,26 +16,6 @@ export function handleNewVaultAdded(event: NewVaultAdded): void {
   if (vault == null) {
     vault = new Vault(vaultId)
   }
-  vault.save()
-}
-
-export function handleTokenBalanceSet(event: TokenBalanceSet): void {
-  let vaultId = event.params.vaultId.toHex()
-  let vault = Vault.load(vaultId)
-  if (vault == null) {
-    vault = new Vault(vaultId)
-  }
-  vault.tokenBalance = event.params._tokenBalance
-  vault.save()
-}
-
-export function handleEthBalanceSet(event: EthBalanceSet): void {
-  let vaultId = event.params.vaultId.toHex()
-  let vault = Vault.load(vaultId)
-  if (vault == null) {
-    vault = new Vault(vaultId)
-  }
-  vault.ethBalance = event.params._ethBalance
   vault.save()
 }
 
@@ -66,35 +53,37 @@ export function handleNftAddressSet(event: NftAddressSet): void {
 export function handleMint(event: Mint): void {
   let vaultId = event.params.vaultId.toHex()
   let mintId = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  let mint = MintEntity.load(mintId)
-  if (mint == null) {
-    mint = new MintEntity(mintId)
-  }
   let vault = Vault.load(vaultId)
   if (vault == null) {
     vault = new Vault(vaultId)
   }
-  mint.vaultId = vaultId
-  mint.sender = event.params.sender
-  mint.nftIds = event.params.nftIds
-  mint.save()
+  let mint = MintEntity.load(mintId)
+  if (mint == null) {
+    mint = new MintEntity(mintId)
+  }
   vault.save()
+  mint.nftIds = event.params.nftIds
+  mint.sender = event.params.sender
+  mint.vault = vault.id
+  mint.save()
+  
+  
 }
 
 export function handleRedeem(event: Redeem): void {
   let vaultId = event.params.vaultId.toHex()
   let redeemId = event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  let redeem = RedeemEntity.load(redeemId)
-  if (redeem == null) {
-    redeem = new RedeemEntity(redeemId)
-  }
   let vault = Vault.load(vaultId)
   if (vault == null) {
     vault = new Vault(vaultId)
   }
-  redeem.vaultId = vaultId
+  vault.save()
+  let redeem = RedeemEntity.load(redeemId)
+  if (redeem == null) {
+    redeem = new RedeemEntity(redeemId)
+  }
+  redeem.vault = vault.id
   redeem.sender = event.params.sender
   redeem.nftIds = event.params.nftIds
   redeem.save()
-  vault.save()
 }
